020de92e3a35097d701ebcf6e97c721a3438a26a
anishaas <anisha.srivastava123@gmail.com>
2015-10-28 11:28:33 -0500

    Login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ def new #new login session
 end
 
 def create
-  user = Student.find_by_email(params[:email])
+  user = Student.find_by_username(params[:username])
   if user && user.authenticate(params[:password])
     set_user_session(user) #set session's user id to params passed in user
-    redirect_to root_path, notice: "Successfully Logged In. Welcome! #{current_user.name}"
+    redirect_to root_path, notice: "Successfully Logged In. Welcome! #{current_user.first_name + current_user.last_name}"
   else
     flash[:alert] = "Login Credentials Invalid!"
     render :new #back to login page
