8931b2cbb0ab2b7c26ef81640f50c74bad46e3d2
anishaas <anisha.srivastava123@gmail.com>
2015-11-02 16:31:47 -0600

    Added to student dashboard

--- app/controllers/student_dashboard_controller.rb
+++ app/controllers/student_dashboard_controller.rb
@@ -2,6 +2,7 @@ class StudentDashboardController < ApplicationController
   before_filter :authenticate_user
   def index
     @courses = current_user.courses
+    @submissions = current_user.submissions
     # @graded_submissions = current_user.submissions.where(graded: true)
   end
 end
