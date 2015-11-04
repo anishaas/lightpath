66443d737f30af478c19ed5b41737f5b29548196
anishaas <anisha.srivastava123@gmail.com>
2015-11-04 15:47:53 -0600

    Deleted Teacher sidebar code

--- app/assets/javascripts/components/studentslightbulbs.js.jsx
+++ app/assets/javascripts/components/studentslightbulbs.js.jsx
@@ -1,15 +1,1 @@
-var StudentLightbulbsbar = React.createClass({
-  render: function () {
-    var course_id = this.props.course.id;
-    var lightbulbs = this.props.lightbulbs;
-      return (
-        <div className = 'studentslightbulbsbar'>
-          <ul><h4>"Your Students' Lightbulbs"</h4></ul>
-              {lightbulbs.map(function (lightbulb, index)
-                  (<ul key={index}>
-                    <Link url={'/courses/' + course_id +'/lightbulb/' + lightbulb.id} name={lightbulb.summary}/>
-                  </ul>);
-              }
-        </div>);
-      }
-});
+
