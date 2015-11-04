2ba481877c3e27085e9bd52229dd080074ba2904
anishaas <anisha.srivastava123@gmail.com>
2015-11-04 16:06:12 -0600

    Deleted unused JS code

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,6 +1,1 @@
 //= require_tree ./components
-<div id="blanket" style="display:none"></div>
-<div id="popUpDiv" style="display:none">
-
-<a href="#" onclick="popup('popUpDiv')" >Create a Lightbulb</a> </div>
-<a href="#" onclick="popup('popUpDiv')">Click to Open CSS Pop Up</a>
