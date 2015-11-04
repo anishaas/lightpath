e11f9c5eee1fad047bbcaed289bd8d100bf84927
anishaas <anisha.srivastava123@gmail.com>
2015-11-04 15:30:13 -0600

    Closed out div tag in SubmitButton React component

--- app/assets/javascripts/components/submitbutton.js.jsx
+++ app/assets/javascripts/components/submitbutton.js.jsx
@@ -1,47 +1,47 @@
-var SubmitButton = React.createClass({
-  getInitialState: function () {
-    return {
-      submitted: false
-    };
-  },
-
-  handleClick: function (event) {
-    var assignmentid = this.props.assignmentID;
-      $.ajax({
-        url: '/assignments/' + assignmentid + '/submissions',
-        method: "POST",
-        dataType: "JSON",
-        data: {
-          submission: {
-            assignment_id: assignmentid,
-            student_id: this.props.studentID,
-            body: this.state.body,
-          }
-        }
-          }).done(function () {
-          this.setState({ submitted: true });
-        }.bind(this));
-  },
-
-  changebody: function (e) {
-    this.setState({ body: e.target.value });
-  },
-
-render: function () {
-      if (this.state.submitted) {
-          return (<div className=''>Submitted!</div);
-      } else {
-          return(
-          <div className='form'>
-          <label htmlFor='Body'>Submission Content</label>
-          <input id='Article' placeholder={'Submit your response here!'}
-          size="50" type="string"
-          onChange={this.changebody}
-          value={this.state.body}
-          className={this.state.className}/>
-          <div className="btn" onClick={this.handleClick}>Submit Assignment!</div>
-        </div>
-        );
-    }
-  }
-});
+// var SubmitButton = React.createClass({
+//   getInitialState: function () {
+//     return {
+//       submitted: false
+//     };
+//   },
+//
+//   handleClick: function (event) {
+//     var assignmentid = this.props.assignmentID;
+//       $.ajax({
+//         url: '/assignments/' + assignmentid + '/submissions',
+//         method: "POST",
+//         dataType: "JSON",
+//         data: {
+//           submission: {
+//             assignment_id: assignmentid,
+//             student_id: this.props.studentID,
+//             body: this.state.body,
+//           }
+//         }
+//           }).done(function () {
+//           this.setState({ submitted: true });
+//         }.bind(this));
+//   },
+//
+//   changebody: function (e) {
+//     this.setState({ body: e.target.value });
+//   },
+//
+// render: function () {
+//       if (this.state.submitted) {
+//           return (<div className=''>Submitted!</div>);
+//       } else {
+//           return(
+//           <div className='form'>
+//           <label htmlFor='Body'>Submission Content</label>
+//           <input id='Article' placeholder={'Submit your response here!'}
+//           size="50" type="string"
+//           onChange={this.changebody}
+//           value={this.state.body}
+//           className={this.state.className}/>
+//           <div className="btn" onClick={this.handleClick}>Submit Assignment!</div>
+//         </div>
+//         );
+//     }
+//   }
+// });
