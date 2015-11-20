59809715802aa04cf660de0d34c97977c815ec6b
anishaas <anisha.srivastava123@gmail.com>
2015-11-20 12:26:32 -0600

    Added a course

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,16 +17,19 @@ Teacher.create(first_name:"Martin", last_name:"Kevorkian", email: "mk@example.co
 username:"martinkevorkian", admin:true, password: 'password')
 Teacher.create(first_name:"Justin", last_name:"Herrick", email: "jh@example.com",
 username:"justinherrick", admin:true, password: 'password')
+Teacher.create(first_name:"Self-teaching", email: "anisha.srivastava123@gmail.com",
+username:"anishasrivastava", admin:true, password: 'password')
 
 Course.create(name:"World Literature",teacher_id:1, description:"Classic literature")
 Course.create(name:"Backend Engineering, Ruby on Rails", teacher_id:2, description:"Build web applications using Rails framework")
+Course.create(name:"Hindi: Grammar and Conversation", teacher_id:3, description: "Learn correct Hindi grammar and build vocabulary")
 
 Enrollment.create(student_id:1, course_id:1)
 Enrollment.create(student_id:1, course_id:2)
 Enrollment.create(student_id:2, course_id:1)
 Enrollment.create(student_id:3, course_id:2)
 Enrollment.create(student_id:3, course_id:1)
-
+Enrollment.create(student_id:1, course_id:3)
 
 Lightbulb.create(lesson_id: 3, course_id: 1, assignment_id: 1, enrollment_id: 1, student_id: 1, summary:"Ahab -- clash of fate and free-will")
 Lightbulb.create(lesson_id: 3, course_id: 1, assignment_id: 2, enrollment_id: 1, student_id: 1, summary:"Roark represents individualism, Keating conformity.")
